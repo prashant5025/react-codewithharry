f05c7aebb00505b4580b767de41617b29986eff5
Prashant Singh <psingh5025615@gmail.com>
2023-03-06 01:02:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Call</li>
+          <li>meet me</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
