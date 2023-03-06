0cb79fb5757a5931d68a5697f314beea66343748
Prashant Singh <psingh5025615@gmail.com>
2023-03-07 02:54:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
+import React from 'react';
+
+let name = "prashant singh";
+const element = React.createElement('h1', {className: 'greeting'}, 'Hello World');
 
 function App() {
   return (
     <div className='container'>
-      <nav className='nav'>
-        <ul className='nav-links'>
-          <li className=''>Home</li>
-          <li className=''>About</li>
-          <li className=''>Contact</li>
-          <li className=''>Call</li>
-          <li className=''>meet me</li>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>Contact</li>
+          <li>About me</li>
+          <li>Login</li>
         </ul>
       </nav>
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, ducimus est? Laborum magnam, esse eos dolores quos ea numquam, temporibus explicabo, voluptates quam hic saepe. Perspiciatis, nulla accusantium! Aut cumque adipisci eos? Voluptatum, dolorem.</p>
     </div>
+
   );
 }
 
